a290b5aa0953d2f3b41c2721d5f5af0f981e72b1
pangurban <pangurban.nn@gmail.com>
2016-06-26 17:04:59 +0400

    Реализован экран приветствия.

---
+++ source/js/app.js
@@ -0,0 +1,55 @@
+(function() {
+  'use strict';
+
+  var states = {
+    SIGN_IN: 'sign-in',
+    SIGN_UP: 'sign-up'
+  };
+
+  var forms = {
+    [states.SIGN_IN]: {
+      title: 'Добро пожаловать',
+      el: document.forms['sign-in'],
+      actionIn: function() {
+        rootEl.querySelector('.welcome_description').hidden = false;
+
+      },
+      actionOut: function() {
+        rootEl.querySelector('.welcome_description').hidden = true;
+      }
+    },
+    [states.SIGN_UP]: {
+      title: 'Регистрация',
+      el: document.forms['sign-up']
+    }
+  };
+
+  var rootEl = document.querySelector('.welcome');
+  var titleEl = rootEl.querySelector('.welcome_title');
+  var activeState = states.SIGN_IN;
+
+  function changeState(newState) {
+    forms[activeState].actionOut && forms[activeState].actionOut();
+    forms[newState].actionIn && forms[newState].actionIn();
+
+    forms[activeState].el.hidden = true;
+    forms[newState].el.hidden = false;
+    titleEl.textContent = forms[newState].title;
+
+    activeState = newState;
+  }
+
+  document.querySelector('.welcome').addEventListener('click', function(e) {
+    var target = e.target;
+    var behavior = target.dataset.behavior;
+
+    if (!behavior) {
+      return;
+    }
+
+    switch (behavior) {
+      case 'view-sign-in': changeState(states.SIGN_IN); break;
+      case 'view-sign-up': changeState(states.SIGN_UP); break;
+    }
+  });
+})();
